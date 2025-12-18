573f2704b174755743d56ab06e47ccd7116a9e79
KarlStevenRequinto <lerdrobott@gmail.com>
2025-12-19 01:29:20 +0800

    adding database

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
-import { auth } from "@/lib/better-auth/auth";
+
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { auth } from "@/lib/better-auth/auth";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
     const session = await auth.api.getSession({ headers: await headers() });
